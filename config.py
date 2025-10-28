778a5734bae20dbe36c14a590f6072046b48e2a8
Mohammed Irfan <mailtomohammedirfank@gmail.com>
2025-10-28 14:24:11 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    CHUNK_SIZE = 800
-    CHUNK_OVERLAP = 100
-    EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
-    GROK_API_KEY = os.getenv("GROK_API_KEY")
